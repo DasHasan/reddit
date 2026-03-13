7ef5332c84ac49e0b9613fb9cdae3762886ba0f0
Hasan <hasan.celik1501@gmail.com>
2026-03-13 23:48:55 +0100

    Send over18=1 cookie in worker to unblock NSFW subreddits

--- worker.js
+++ worker.js
@@ -18,6 +18,7 @@ export default {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
                 'Accept': 'application/json',
+                'Cookie': 'over18=1',
             },
         });
 
